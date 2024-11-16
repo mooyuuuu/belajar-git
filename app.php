f90880e2c1f46f54e55dd73569f82f72ee7c4a26
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2024-11-16 21:08:24 +0800

    Menambahkah function sayOk di app.php

--- app.php
+++ app.php
@@ -4,4 +4,10 @@ require_once __DIR__ . "/index.php";
 
 use function index\sayHello;
 
+function sayOk(string $name): void 
+{
+    echo "it`s ok $name" . PHP_EOL;
+}
+
 sayHello('joko');
+sayOk('yusfi');
